303764f2ca743fe68cad4a043494fb89eb7ed7ae
chaimabaraket <barket166@gmail.com>
2022-12-14 03:17:46 +0100

    src

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AddProduitComponent } from './add-produit/add-produit.component';
+import { ProduitsComponent } from './produits/produits.component';
+import { RechercheParCategorieComponent } from './recherche-par-categorie/recherche-par-categorie.component';
+import { RechercheParNomComponent } from './recherche-par-nom/recherche-par-nom.component';
+import { UpdateProduitComponent } from './update-produit/update-produit.component';
+
+
+
+const routes: Routes = [
+  { path: "produits", component: ProduitsComponent },
+  { path: "add-produit", component: AddProduitComponent },
+  { path: "updateProduit/:id", component: UpdateProduitComponent },
+  { path: "", redirectTo: "produits", pathMatch: "full" },
+  { path: "rechercheParNom", component: RechercheParNomComponent },
+  { path: "rechercheParCategorie", component: RechercheParCategorieComponent }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
